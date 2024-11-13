22b8142cec51647286e87baedc37c459cb1a53b0
gmh5225 <2315157@qq.com>
2024-11-13 21:39:26 +0800

    building

--- src/V2Dex.sol
+++ src/V2Dex.sol
@@ -5,12 +5,31 @@ import { Address } from "@openzeppelin/contracts/utils/Address.sol";
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 import { ReentrancyGuardTransient } from "@openzeppelin/contracts/utils/ReentrancyGuardTransient.sol";
 import { Pausable } from "@openzeppelin/contracts/utils/Pausable.sol";
+import { UniswapV2Factory } from "@uniswap-v2/core/UniswapV2Factory.sol";
+import { UniswapV2Router02 } from "@uniswap-v2/periphery/UniswapV2Router02.sol";
 
 contract V2Dex is Ownable, ReentrancyGuardTransient, Pausable {
-    address public admin;
+    UniswapV2Factory public immutable factory;
+    UniswapV2Router02 public immutable router;
+    address public immutable weth;
+    address public immutable admin;
 
-    constructor(address initialAdmin) Ownable(initialAdmin) {
+    constructor(address initialAdmin, address _weth) Ownable(initialAdmin) {
         admin = initialAdmin;
+        weth = _weth;
+
+        factory = new UniswapV2Factory(initialAdmin);
+        router = new UniswapV2Router02(address(factory), weth);
+    }
+
+    // get pair
+    function getPair(address tokenA, address tokenB) external view returns (address) {
+        return factory.getPair(tokenA, tokenB);
+    }
+
+    // create pair
+    function createPair(address tokenA, address tokenB) external returns (address) {
+        return factory.createPair(tokenA, tokenB);
     }
 
     // Receive ETH
